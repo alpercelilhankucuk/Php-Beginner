eea9999a25d86d271f92cf7dd988cfff74935433
Alper Celilhan Küçük <alpercelilhankucuk@192.168.1.90>
2022-12-25 20:14:46 +0300

    First commit

---
+++ Baslangic/index.php
@@ -0,0 +1,23 @@
+<!--
+Php Etiketleri
+Echo Print Komutları
+Komut Ayrımı
+Açıklama Satırları
+-->
+<h1>Alper Celilhan Küçük</h1>
+
+<?php
+
+//PHP AÇIKLAMA SATIRI
+#PHP ACIKLAMA SATIRI
+/**/
+echo 'Alper celilhan küçük echo';
+print "alper print";
+<<<HTML
+<br>
+>>> HTML
+print "alper celilhan küçük"
+HTML;
+?>
+
+<?= 'echonun kısa kullanımı' ?>
